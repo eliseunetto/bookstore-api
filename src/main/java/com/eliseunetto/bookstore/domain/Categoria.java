a64488031d0c1b8636395ed54070af3679538a84
eliseunetto <eliseunetto@hotmail.com>
2021-04-30 21:13:27 -0300

    Criando relacionamento (Categoria e Livro) e add anotacoes

--- src/main/java/com/eliseunetto/bookstore/domain/Categoria.java
+++ src/main/java/com/eliseunetto/bookstore/domain/Categoria.java
@@ -1,17 +1,30 @@
 package com.eliseunetto.bookstore.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	private String descricao;
+	
+	@OneToMany(mappedBy = "categoria")
+	private List<Livro> livros = new ArrayList<>();
 
 	public Categoria() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	public Categoria(Integer id, String nome, String descricao) {
@@ -43,6 +56,14 @@ public class Categoria implements Serializable {
 
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
+	}
+	
+	public List<Livro> getLivros() {
+		return livros;
+	}
+	
+	public void setLivros(List<Livro> livros) {
+		this.livros = livros;
 	}
 
 	@Override
--- src/main/java/com/eliseunetto/bookstore/domain/Livro.java
+++ src/main/java/com/eliseunetto/bookstore/domain/Livro.java
@@ -2,24 +2,39 @@ package com.eliseunetto.bookstore.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Livro implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String titulo;
 	private String autor;
 	private String texto;
 
+	@ManyToOne
+	@JoinColumn
+	private Categoria categoria;
+
 	public Livro() {
 		super();
 	}
 
-	public Livro(Integer id, String titulo, String autor, String texto) {
+	public Livro(Integer id, String titulo, String autor, String texto, Categoria categoria) {
 		super();
 		this.id = id;
 		this.titulo = titulo;
 		this.autor = autor;
 		this.texto = texto;
+		this.categoria = categoria;
 	}
 
 	public Integer getId() {
@@ -52,6 +67,14 @@ public class Livro implements Serializable {
 
 	public void setTexto(String texto) {
 		this.texto = texto;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
 	}
 
 	@Override
