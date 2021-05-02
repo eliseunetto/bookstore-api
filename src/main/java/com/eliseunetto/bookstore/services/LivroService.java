36251bfb0cc0928542f22f7000cdaae3b60841ad
eliseunetto <eliseunetto@hotmail.com>
2021-05-02 18:47:50 -0300

    Metodo add para Livro

--- src/main/java/com/eliseunetto/bookstore/services/LivroService.java
+++ src/main/java/com/eliseunetto/bookstore/services/LivroService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.eliseunetto.bookstore.domain.Categoria;
 import com.eliseunetto.bookstore.domain.Livro;
 import com.eliseunetto.bookstore.repositories.LivroRepository;
 import com.eliseunetto.bookstore.services.exceptions.ObjectNotFoundException;
@@ -46,5 +47,12 @@ public class LivroService {
 		if(obj.getTexto() != null) {
 			newObj.setTexto(obj.getTexto());
 		}
+	}
+	
+	public Livro create(Integer id_cat, Livro obj) {
+		obj.setId(null);
+		Categoria cat = categoriaService.findById(id_cat);
+		obj.setCategoria(cat);
+		return repo.save(obj);
 	}
 }
