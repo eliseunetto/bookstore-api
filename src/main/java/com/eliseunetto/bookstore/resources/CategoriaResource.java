491a6c7065eafa9f57b9fbdbc1681b7fa1d649d6
eliseunetto <eliseunetto@hotmail.com>
2021-05-02 01:26:49 -0300

    Metodo add para Categoria

--- src/main/java/com/eliseunetto/bookstore/resources/CategoriaResource.java
+++ src/main/java/com/eliseunetto/bookstore/resources/CategoriaResource.java
@@ -1,5 +1,6 @@
 package com.eliseunetto.bookstore.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.eliseunetto.bookstore.domain.Categoria;
 import com.eliseunetto.bookstore.dtos.CategoriaDTO;
@@ -32,5 +36,16 @@ public class CategoriaResource {
 		List<Categoria> list = service.findAll();
 		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@PostMapping()
+	public ResponseEntity<Categoria> create(@RequestBody Categoria obj) {
+		Categoria categoria = service.create(obj);
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(categoria.getId())
+				.toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/eliseunetto/bookstore/services/CategoriaService.java
+++ src/main/java/com/eliseunetto/bookstore/services/CategoriaService.java
@@ -25,4 +25,9 @@ public class CategoriaService {
 	public List<Categoria> findAll() {
 		return repo.findAll();
 	}
+	
+	public Categoria create(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
 }
