8184054989d0c01ffacffb46ad6f358311719d7d
eliseunetto <eliseunetto@hotmail.com>
2021-05-01 22:03:45 -0300

    Tratamento de erro personalizado para Categoria

--- src/main/java/com/eliseunetto/bookstore/services/CategoriaService.java
+++ src/main/java/com/eliseunetto/bookstore/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.eliseunetto.bookstore.domain.Categoria;
 import com.eliseunetto.bookstore.repositories.CategoriaRepository;
+import com.eliseunetto.bookstore.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,7 @@ public class CategoriaService {
 
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ". Tipo: " + Categoria.class.getName()));
 	}
 }
